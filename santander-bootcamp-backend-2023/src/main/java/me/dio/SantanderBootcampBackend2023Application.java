3df18596422fd010395b062f6de2e0e240e82638
Daniel de Andrade Pereira <05danielandrade@gmail.com>
2023-10-19 01:24:41 -0300

    first commit

---
+++ santander-bootcamp-backend-2023/src/main/java/me/dio/SantanderBootcampBackend2023Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderBootcampBackend2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderBootcampBackend2023Application.class, args);
+	}
+
+}
